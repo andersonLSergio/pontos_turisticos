30b78aad7de3ab86691b61ed89061922826ec9a6
Anderson Luis Sergio <anderson.als04@gmail.com>
2018-09-19 16:30:38 -0300

    Added atracoes endpoint

--- pontos_turisticos/pontos_turisticos/urls.py
+++ pontos_turisticos/pontos_turisticos/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
+from atracoes.api.viewsets import AtracoesViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
+router.register(r'atracoes', AtracoesViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
