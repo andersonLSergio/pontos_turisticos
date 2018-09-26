9af101486aea1fda0c006e733ad839166f7c9632
Anderson Luis Sergio <anderson.als04@gmail.com>
2018-09-25 22:00:40 -0300

    Default actions overriding in PontoTuristico endpoint

--- pontos_turisticos/core/api/viewsets.py
+++ pontos_turisticos/core/api/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
 from .serializers import PontoTuristicoSerializer
 from core.models import PontoTuristico
@@ -12,3 +13,27 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
+
+    #GET ACTION
+    def list(self, request, *args, **kwargs):
+        return Response({'teste': 123})
+
+    #POST ACTION
+    def create(self, request, *args, **kwargs):
+        return Response({'Hello': request.data['nome']})
+
+    #DELETE ACTION
+    def destroy(self, request, *args, **kwargs):
+        pass
+
+    #GET ACTION FOR UNIC RESULT
+    def retrieve(self, request, *args, **kwargs):
+        pass
+
+    #PUT ACTION
+    def update(self, request, *args, **kwargs):
+        pass
+
+    #PATCH ACTION
+    def partial_update(self, request, *args, **kwargs):
+        pass
